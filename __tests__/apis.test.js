57cd83b9219d05a24e8417ba809fbeb7f7e86a6e
Sarah <skins1.anderson@gmail.com>
2022-11-15 08:58:42 +0000

    get review by review id

--- __tests__/apis.test.js
+++ __tests__/apis.test.js
@@ -1,24 +1,24 @@
-const request = require('supertest')
+const request = require("supertest");
 
-const app = require('../app.js')
+const app = require("../app.js");
 const db = require("../db/connection.js");
 const seed = require("../db/seeds/seed.js");
 const data = require("../db/data/test-data");
 
 beforeEach(() => {
-    return seed(data);
+  return seed(data);
 });
 
 afterAll(() => {
-    return db.end();
+  return db.end();
 });
 
-describe('1. GET /api/categories', () => {
-  test('status: 200, responds with an array of category objects including slugs and descriptions', () => {
+describe("1. GET /api/categories", () => {
+  test("status: 200, responds with an array of category objects including slugs and descriptions", () => {
     return request(app)
-      .get('/api/categories')
+      .get("/api/categories")
       .expect(200)
-      .then(({ body: {categories} }) => {
+      .then(({ body: { categories } }) => {
         expect(categories.length).toBe(4);
         categories.forEach((category) => {
           expect(category).toEqual(
@@ -32,12 +32,12 @@ describe('1. GET /api/categories', () => {
   });
 });
 
-describe('2. GET /api/reviews', () => {
-  test('status: 200, responds with an array of review objects', () => {
+describe("2. GET /api/reviews", () => {
+  test("status: 200, responds with an array of review objects", () => {
     return request(app)
-      .get('/api/reviews')
+      .get("/api/reviews")
       .expect(200)
-      .then(({ body: {reviews} }) => {
+      .then(({ body: { reviews } }) => {
         expect(reviews.length).toBe(13);
         reviews.forEach((review) => {
           expect(review).toEqual(
@@ -56,15 +56,52 @@ describe('2. GET /api/reviews', () => {
         });
       });
   });
-  test('should sort the reviews by descending date', () => {
+  test("should sort the reviews by descending date", () => {
     return request(app)
-    .get('/api/reviews')
-    .expect(200)
-    .then(({ body: {reviews} }) => {
-      expect(reviews).toBeSortedBy('created_at', {
-        descending: true
-      })
-    })
+      .get("/api/reviews")
+      .expect(200)
+      .then(({ body: { reviews } }) => {
+        expect(reviews).toBeSortedBy("created_at", {
+          descending: true,
+        });
+      });
   });
 });
 
+describe("3. GET /api/reviews/:review_id", () => {
+  test("200 - responds with a single object with correct information", () => {
+    request(app)
+      .get("/api/reviews/1")
+      .expect(200)
+      .then(({ body: { review } }) => {
+        expect(review).toMatchObject({
+          review_id: 1,
+          title: "Agricola",
+          category: "euro game",
+          designer: "Uwe Rosenberg",
+          owner: "mallionaire",
+          review_body: "Farmyard fun!",
+          review_img_url:
+            "https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png",
+          created_at: "2021-01-18T10:00:20.514Z",
+          votes: 1,
+        });
+      });
+  });
+  test("404 - responds with a not found message when a non-existing review is requested", () => {
+    return request(app)
+      .get("/api/reviews/999")
+      .expect(404)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe("Review 999 not found");
+      });
+  });
+  test("400 - responds with an error message because review_id is not a number", () => {
+    return request(app)
+      .get("/api/reviews/sdkjldkjfk")
+      .expect(400)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe("invalid id");
+      });
+  });
+});
--- controllers/gamesC.js
+++ controllers/gamesC.js
@@ -1,27 +1,36 @@
 const {
-    selectCategories,
-    selectReviews,
+  selectCategories,
+  selectReviews,
+  selectReviewById,
 } = require("../models/gamesM.js");
 
 exports.getCategories = (req, res, next) => {
-    selectCategories()
-        .then((categories) => {
-            res.status(200).send({ categories: categories });
-        })
-        .catch((err) => {
-            next(err);
-        });
+  selectCategories()
+    .then((categories) => {
+      res.status(200).send({ categories: categories });
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
 
 exports.getReviews = (req, res, next) => {
-    selectReviews()
-        .then((reviews) => {
-            res.status(200).send({ reviews: reviews });
-        })
-        .catch((err) => {
-            next(err);
-        });
+  selectReviews()
+    .then((reviews) => {
+      res.status(200).send({ reviews: reviews });
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
 
-
-
+exports.getReviewById = (req, res, next) => {
+  const { review_id } = req.params;
+  selectReviewById(review_id)
+    .then((review) => {
+      res.status(200).send({ review: review });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
